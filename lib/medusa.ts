d35d8a31e698a95702d6b5397622cd675d3aacbe
Albertina <alber.pezzutti@gmail.com>
2026-04-20 03:40:01 -0300

    Arreglé un url

--- lib/medusa.ts
+++ lib/medusa.ts
@@ -1,6 +1,6 @@
 import Medusa from "@medusajs/js-sdk"
 
 export const medusa = new Medusa({
-  baseUrl: "http://localhost:9000",
+  baseUrl: process.env.NEXT_PUBLIC_MEDUSA_URL || "http://localhost:9000",
   publishableKey: "pk_9fb79533c0b334820e6296a8cb165fe05650abb75be4d87f944d76fc77d28910",
 })
